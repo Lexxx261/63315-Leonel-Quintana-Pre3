03a0d79c6cde3abd7f5ce77bfd70b28e4d732ccc
Leonel Quintana <leonelquint@gmail.com>
2024-11-12 21:53:43 -0300

    Carga de pre entrega 2

---
+++ scripts/productos.js
@@ -0,0 +1,20 @@
+// "base de prod"
+export const productos = [
+    { id: 1, nombre: "Combo Frio", precio: 500, categoria: "Box" },
+    { id: 2, nombre: "Combo Caliente", precio: 600, categoria: "Box" },
+    { id: 3, nombre: "Combo Premium", precio: 1000, categoria: "Box" },
+
+    { id: 4, nombre: "Rogel", precio: 1100, categoria: "Tortas" },
+    { id: 5, nombre: "Chessscake Frutos Rojos", precio: 850, categoria: "Tortas" },
+    { id: 6, nombre: "Chajá", precio: 1100, categoria: "Tortas" },
+    { id: 7, nombre: "Mini Cake con Diseño", precio: 400, categoria: "Tortas" },
+    { id: 8, nombre: "Torta 3kg con Diseño", precio: 20, categoria: "Tortas" },
+
+    { id: 9, nombre: "Combo 1", precio: 390, categoria: "Desayunos" },
+    { id: 10, nombre: "Combo 2", precio: 900, categoria: "Desayunos" },
+    { id: 11, nombre: "Combo 3", precio: 1300, categoria: "Desayunos" },
+
+    { id: 12, nombre: "Mix Shots (12un.)", precio: 600, categoria: "Eventos" },
+    { id: 13, nombre: "Corazones Rellenso (6un.)", precio: 390, categoria: "Eventos" },
+    { id: 14, nombre: "Mini Donas (12un.)", precio: 230, categoria: "Eventos" },
+];
